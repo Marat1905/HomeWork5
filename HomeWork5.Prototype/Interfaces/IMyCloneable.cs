ee62cd8754ed154ee8a6ab84d7891f7dc3b8af7b
Marat <milya010790@mail.ru>
2025-02-04 23:34:54 +0500

    Создание проекта

---
+++ HomeWork5.Prototype/Interfaces/IMyCloneable.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeWork5.Prototype.Interfaces
+{
+    public interface IMyCloneable<T>  where T : class
+    {
+        T DeepClone();
+    }
+}
---
+++ HomeWork5.Prototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using HomeWork5.Prototype.Implementations;
+
+namespace HomeWork5.Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyCloneableBase a = new MyCloneableBase() { A = 10 , B = 20};
+
+            var b = a.DeepClone();
+
+            Console.WriteLine(b);
+
+           Console.ReadLine();
+        }
+    }
+}
