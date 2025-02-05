195f2480d2a5a127cb7fe61b7d444c5607779b57
Marat <milya010790@mail.ru>
2025-02-05 15:28:16 +0500

    добавил ссылку от куда был взят код

--- HomeWork5.Prototype/Extensions/CopyHelper.cs
+++ HomeWork5.Prototype/Extensions/CopyHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 
+//https://ru.stackoverflow.com/questions/537280/%D0%9F%D0%BE%D0%BB%D0%BD%D0%B0%D1%8F-%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B0
 namespace HomeWork5.Prototype.Extensions
 {
     public static class CopyHelper
