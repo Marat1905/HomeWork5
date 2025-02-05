14ca08b6b66e9bc3d9eeb643b5d8c608d27e5201
Marat <milya010790@mail.ru>
2025-02-05 11:54:16 +0500

    Добавлен проект для тестирования

---
+++ HomeWork5.Prototype.Tests/PersonTests.cs
@@ -0,0 +1,49 @@
+﻿using HomeWork5.Prototype.Implementations;
+using System;
+using System.Text.Json;
+
+namespace HomeWork5.Prototype.Tests
+{
+    
+    public class PersonTests
+    {
+        [Fact]
+        public void MyCloneableTest_MustReturnPersonWithEqualFields()
+        {
+            // Arrange
+            var person = new Person("Maks", 20);
+            var clone = person.MyClone();
+
+            // Act
+
+            // Assert
+            Assert.Equal(JsonSerializer.Serialize(person), JsonSerializer.Serialize(clone));
+        }
+
+        [Fact]
+        public void CloneableTest_MustReturnPersonWithEqualFields()
+        {
+            // Arrange
+            var person = new Person("Maks", 20);
+            var clone = (Person)person.Clone();
+
+            // Act
+
+            // Assert
+            Assert.Equal(JsonSerializer.Serialize(person), JsonSerializer.Serialize(clone));
+        }
+
+        [Fact]
+        public void DeepCloneableTest_MustReturnPersonWithEqualFields()
+        {
+            // Arrange
+            var person = new Person("Maks", 20);
+            var clone = person.DeepCopy();
+
+            // Act
+
+            // Assert
+            Assert.Equal(JsonSerializer.Serialize(person), JsonSerializer.Serialize(clone));
+        }
+    }
+}
