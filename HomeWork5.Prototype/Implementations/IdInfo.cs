164164b3f4a8c3441b90abc9f9e3c901f44955b7
Marat <milya010790@mail.ru>
2025-02-06 09:52:11 +0500

    Объект для вложения

--- HomeWork5.Prototype/Implementations/IdInfo.cs
+++ HomeWork5.Prototype/Implementations/IdInfo.cs
@@ -1,9 +1,6 @@
-﻿using HomeWork5.Prototype.Extensions;
-using HomeWork5.Prototype.Interfaces;
-
-namespace HomeWork5.Prototype.Implementations
+﻿namespace HomeWork5.Prototype.Implementations
 {
-    public class IdInfo: IMyCloneable<IdInfo>,ICloneable
+    public class IdInfo
     {
         public string Type {  get; set; }
         public int IdNumber { get; set; }
@@ -13,29 +10,5 @@ namespace HomeWork5.Prototype.Implementations
             IdNumber = idNumber;
             Type = type;
         }
-
-        public IdInfo(IdInfo idInfo)
-        {
-            IdNumber = idInfo.IdNumber;
-            Type = idInfo.Type;
-        }
-
-
-        public object Clone()
-        {
-            return MemberwiseClone();
-        }
-
-        public IdInfo MyClone()
-        {
-            return new IdInfo(this);
-        }
-
-        public IdInfo DeepCopy()
-        {
-            return CopyHelper.CreateDeepCopy(this);
-        }
-
-       
     }
 }
