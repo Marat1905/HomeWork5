4623bcf6b88040bc9b7b79ee22f01389e6bd27ef
Marat <milya010790@mail.ru>
2025-02-06 09:51:37 +0500

    Добавлено описание

--- HomeWork5.Prototype/Interfaces/IMyCloneable.cs
+++ HomeWork5.Prototype/Interfaces/IMyCloneable.cs
@@ -1,9 +1,15 @@
 ﻿namespace HomeWork5.Prototype.Interfaces
 {
+    /// <summary>Кастомный интерфейс для клонирования объекта</summary>
+    /// <typeparam name="T">Тип класса</typeparam>
     public interface IMyCloneable<T>  where T : class
     {
+        /// <summary>Поверхностное клонирование</summary>
+        /// <returns>Новый объект, если есть вложенные объекты то они передаются по ссылке</returns>
         T MyClone();
 
+        /// <summary>Глубокое клонирование</summary>
+        /// <returns>Новый объект</returns>
         T DeepCopy();
     }
 }
